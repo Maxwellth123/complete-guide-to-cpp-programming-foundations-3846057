ac7114d9a6be7964e3176c7ba37eb053acd83a00
Maxwellth123 <Maxwell.Thompson@landisgyr.com>
2025-02-25 16:33:28 +0000

    added comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,6 +1,6 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 00_03
-// Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
+// Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño and Max
 
 #include <iostream>
 
